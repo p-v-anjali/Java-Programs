378e525f6e80c89574ccd814579db7d9752e7d54
Anjali <140676692+p-v-anjali@users.noreply.github.com>
2024-06-16 18:29:49 +0400

    Program creates two tables, department and EMP, in a MySQL database.

---
+++ CreateTablejava.java
@@ -0,0 +1,20 @@
+import java.sql.*;
+import java.io.*;
+class CreateTable {
+    public static void main(String args[]) {
+       
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/laravel_curd?characterEncoding=utf8","root", "");
+            Statement st = con.createStatement();
+            String str1 = "CREATE TABLE department(dno INT primary key, dname VARCHAR(40),dloc VARCHAR(50))";
+            String str2 = "CREATE TABLE EMP(Eno INT primary key, Ename VARCHAR(40),esal DECIMAL(10,2),dno INT, FOREIGN KEY (dno) REFERENCES department(dno))";
+            System.out.println(str1);
+            st.executeUpdate(str1);
+            System.out.println(str2);
+            st.executeUpdate(str2);
+        } catch (Exception e) {
+            System.out.println("Error :" + e);
+        }
+    }
+}
